d97765b136d2de1bf2277a5d6f290f641bde4661
Dossend <ldonneys10@gmail.com>
2024-11-06 21:27:38 -0500

    Create Compra.js

---
+++ js/Compra.js
@@ -0,0 +1,24 @@
+function playAudio() {
+
+    const audio = document.getElementById("music");
+    const gifContainer = document.getElementById("gif-container");
+  
+    if (audio.paused) {
+      audio.play();
+      gifContainer.style.display = "block"; // Muestra el GIF
+    } else {
+      audio.pause();
+      gifContainer.style.display = "none"; // Oculta el GIF
+    }
+  
+  }
+  
+  function formatearNumero(input) {
+  
+    let value = input.value.replace(/\D/g, ""); 
+    let formattedValue = new Intl.NumberFormat("es-CO").format(value); 
+  
+    input.value = "$" + formattedValue; 
+    
+  }
+  
