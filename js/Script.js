264113cbd6d030cce82936cd77e73157ece80870
Dossend <ldonneys10@gmail.com>
2024-11-09 14:59:48 -0500

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -1,48 +1,40 @@
-const consoleText = (words, id, colors = ["#fff"]) => {
-    
-    let visible = true;
-    const con = document.getElementById("console");
-    let letterCount = 1;
-    let x = 1;
-    let waiting = false;
-    const target = document.getElementById(id);
-    target.setAttribute("style", "color:" + colors[0]);
+function playAudio() {
+  const audio = document.getElementById("music");
+  const gifContainer = document.getElementById("gif-container");
 
-    setInterval(() => {
-        if (letterCount === 0 && !waiting) {
-            waiting = true;
-            target.innerHTML = words[0].substring(0, letterCount);
-            setTimeout(() => {
-                const usedColor = colors.shift();
-                colors.push(usedColor);
-                const usedWord = words.shift();
-                words.push(usedWord);
-                x = 1;
-                target.setAttribute("style", "color:" + colors[0]);
-                letterCount += x;
-                waiting = false;
-            }, 1000);
-        } else if (letterCount === words[0].length + 1 && !waiting) {
-            waiting = true;
-            setTimeout(() => {
-                x = -1;
-                letterCount += x;
-                waiting = false;
-            }, 1000);
-        } else if (!waiting) {
-            target.innerHTML = words[0].substring(0, letterCount);
-            letterCount += x;
-        }
-    }, 120);
+  if (audio.paused) {
+    audio.play();
+    gifContainer.style.display = "block"; // Muestra el GIF
+  } else {
+    audio.pause();
+    gifContainer.style.display = "none"; // Oculta el GIF
+  }
+}
 
-    setInterval(() => {
-        con.classList.toggle("hidden");
-        visible = !visible;
-    }, 400);
-};
+function formatearNumero(input) {
+  let value = input.value.replace(/\D/g, "");
+  let formattedValue = new Intl.NumberFormat("es-CO").format(value);
 
-consoleText(["Welcome.", "Designed by Leandro Donneys G", "¡Loading!"], "text", ["#00CDAC", "#EB3349", "#F09819"]);
+  input.value = "$" + formattedValue;
+}
 
-setTimeout(() => {
-    window.location.href = '/html/Registro.html'; 
-}, 16000);
+function Comprar() {
+  const nombre = document.getElementById("nombre").value.trim();
+  const precio = document.getElementById("precio").value.trim();
+  const cantidad = document.getElementById("cantidad").value.trim();
+  const direccion = document.getElementById("direccion").value.trim();
+  const entregaDomicilio = document.getElementById("domicilio").checked;
+  const entregaPuntoFisico = document.getElementById("puntoFisico").checked;
+
+  if (
+    nombre &&
+    precio &&
+    cantidad &&
+    direccion &&
+    (entregaDomicilio || entregaPuntoFisico)
+  ) {
+    window.location.href = "/html/Loader.html";
+  } else {
+    alert("Por favor, rellene todos los campos correctamente.");
+  }
+}
